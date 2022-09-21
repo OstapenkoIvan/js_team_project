a89cb8602c163c5bf4a41ffde18300dca7890f1e
OstapenkoIvan <ostapenko.ivan@gmail.com>
2022-09-21 09:42:22 +0300

    upd

--- src/js/eventsAPI.js
+++ src/js/eventsAPI.js
@@ -42,17 +42,16 @@ class EventsAPI {
         : response.data;
       return data;
     } catch (error) {
-        if (error.response) {
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          } else if (error.request) {
-            console.log(error.request);
-          } else {
-            console.log('Error', error.message);
-          }
-          console.log(error.config);
-        }
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
     }
   }
 
